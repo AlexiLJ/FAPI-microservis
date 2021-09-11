fb58e7dfe855ffd69d972eac853495a8ba16fcf5
Alex Leiko <leiko.oleksandr@gmail.com>
2021-09-12 00:12:03 +0200

    DEBUG

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 import pathlib
-from fastapi import FastAPI, Request
+import os
+from functools import lru_cache
+from fastapi import FastAPI, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings 
 
+class Settings(BaseSettings):
+    debug: bool = False
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+settings = get_settings()
+
+DEBUG=settings.debug
 BASE_DIR = pathlib.Path(__file__).parent
 templates = Jinja2Templates(directory=str(BASE_DIR/"templates"))
 print((BASE_DIR/"templates").exists())

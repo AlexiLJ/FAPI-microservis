153941598ede334b5f500da08a8784e177cc4a70
Alex Leiko <leiko.oleksandr@gmail.com>
2021-09-12 01:12:11 +0200

    img loader

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 import pathlib
 import os
+import io
+import uuid
 from functools import lru_cache
-from fastapi import FastAPI, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi import (
+    FastAPI,
+    Depends,
+    Request,
+    File,
+    UploadFile
+    )
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings 
 
@@ -20,6 +28,7 @@ settings = get_settings()
 
 DEBUG=settings.debug
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR/"uploaded"
 templates = Jinja2Templates(directory=str(BASE_DIR/"templates"))
 print((BASE_DIR/"templates").exists())
 
@@ -33,3 +42,13 @@ def home_view(request: Request):
 @app.post("/")
 def home_detail_view():
     return {"mamat": "kunem"}
+
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo_view(file:UploadFile=File(...)):
+    bytes_str = io.BytesIO(await file.read())
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix
+    dest = UPLOAD_DIR/f"{uuid.uuid1()}{fext}"
+    with open(str(dest), 'wb') as out:
+        out.write(bytes_str.read())
+    return file
